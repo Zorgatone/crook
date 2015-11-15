ce775fdd62611069161de16f204898d59e7f8bdf
Tommaso Ricci <zorgatone@gmail.com>
2015-11-15 18:37:04 +0100

    Example basic loop

--- src/crook.c
+++ src/crook.c
@@ -6,11 +6,36 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "rlutil/rlutil.h"
 
 int main(void) {
 	printf("Welcome to Crook!\n");
+
+	getch();
+	cls();
+
+	int maxx = tcols(), maxy = trows();
+
+	if (maxx < 0 || maxy < 0) {
+		fprintf(stderr, "Terminal error!\n");
+
+		exit(EXIT_FAILURE);
+	}
+
+	gotoxy(maxx / 2, maxy / 2.0);
+	putchar('@');
+
+	while(getkey() != KEY_ESCAPE) {
+
+	}
+
+	cls();
+	printf("#RIP Player1...\n");
+
+	getch();
+	cls();
 
 	return 0;
 }
